869c586623c6fd2cb125336d73a1e61a07c9a5d0
muri.budiman <muri.budiman@redawning.com>
2016-12-06 11:51:05 +0700

    formInput Checkbox

--- FormInput.php
+++ FormInput.php
@@ -6,20 +6,24 @@ use \Yii;
 use \yii\helpers\Html;
 /*
 
-\udamuri\forminput\FormInput::widget(
-    'model' => $model,
-    'fieldname' => 'username',
-    'type' => '', //checkbox // password // text //
-    'label1'=> 'Label Tes',
-    'label2'=> 'Label Tes',
-    'labeltype' => 'left' , // left, right, middle
-);
+//text input group
+\udamuri\forminput\FormInput::widget([
+                    'input'=>[
+                            'model' => $model,
+                            'fieldname' => 'username',
+                            'type' => 'text', // checkbox, text
+                            'label1'=> 'Label Tes',
+                            'label2'=> 'Label Tes',
+                            'labeltype' => 'left' , // left, right, middle
+                        ]
+                ]);
 
+checkbox button
 \udamuri\forminput\FormInput::widget([
                     'input'=>[
                             'model' => $model,
                             'fieldname' => 'rememberMe',
-                            'type' => 'checkbox', 
+                            'type' => 'checkbox',  // checkbox, text
                         ]
                 ]);
 
@@ -51,77 +55,57 @@ class FormInput extends \yii\base\Widget
                  $label2 = $arrData['label2'];
             }
 
+            $InputId = hTML:: getInputId($model, $fieldname);
+            $InputName = hTML:: getInputName($model, $fieldname);
+
             if($arrData['type'] == 'checkbox')
             {
-                $InputId = hTML:: getInputId($model, $fieldname);
-                $InputName = hTML:: getInputName($model, $fieldname);
-
+                
                 $checked = '';
                 if(isset($model->$fieldname) && $model->$fieldname != 0)
                 {
                     $checked = 'checked="checked"';
                 }
-                
-                $_input = '<div class="input-group"> <div class="form-yii">
-                    <input name="'.$InputName.'" value="0" type="hidden">
-                    <input id="'.$InputId.'" name="'.$InputName.'" value="1"  '.$checked.' type="checkbox">
-                    <label for="'.$InputId.'">'.$label1.'</label>
-                </div></div>';
-            }
-            else if($arrData['type'] == 'radio')
-            {
-                    
-            }
-            else if($arrData['type'] == 'password' && isset($arrData['labeltype']) )
-            {   
-                if($arrData['labeltype'] == 'middle')  
-                {
-                     $_input = '<div class="input-group">
-                                    <span class="input-group-addon">'.$label1.'</span>
-                                    '.Html::activePasswordInput($model, $fieldname).'
-                                    <span class="input-group-addon">'.$label2.'</span>
-                                </div>';
-                }   
-                else if($arrData['labeltype'] == 'right')
-                {
-                    $_input = '<div class="input-group">
-                                  '.Html::activePasswordInput($model, $fieldname).'
-                                  <span class="input-group-addon" id="basic-addon2">'.$label1.'</span>
-                                </div>';
-                }
-                else
-                {
-                    $_input = '<div class="input-group">
-                                  <span class="input-group-addon" id="basic-addon1">'.$label1.'</span>
-                                  '.Html::activePasswordInput($model, $fieldname).'
-                                </div>';
-                }
+
+                $_input = '<div class="form-group field-'.$InputId.'"> 
+                                <div class="form-yii">
+                                    <input name="'.$InputName.'" value="0" type="hidden">
+                                    <input id="'.$InputId.'" name="'.$InputName.'" value="1"  '.$checked.' type="checkbox">
+                                    <label for="'.$InputId.'">'.$label1.'</label>
+                                </div>
+                            </div>';
             }
             else if($arrData['type'] == 'text' && isset($arrData['labeltype']) )
             {
+
                 if($arrData['labeltype'] == 'middle')  
                 {
-                     $_input = '<div class="input-group">
+                     $_input = '<div class="form-group field-'.$InputId.'">
                                     <span class="input-group-addon">'.$label1.'</span>
                                     '.Html::activeTextInput($model, $fieldname).'
                                     <span class="input-group-addon">'.$label2.'</span>
+                                    <p class="help-block"></p>
                                 </div>';
                 }   
                 else if($arrData['labeltype'] == 'right')
                 {
-                    $_input = '<div class="input-group">
-                                  '.Html::activeTextInput($model, $fieldname).'
-                                  <span class="input-group-addon" id="basic-addon2">'.$label1.'</span>
+                    $_input =  '<div class="form-group field-'.$InputId.'">
+                                      '.Html::activeTextInput($model, $fieldname).'
+                                      <span class="input-group-addon" id="basic-addon2">'.$label1.'</span>
+                                      <p class="help-block"></p>
                                 </div>';
                 }
                 else
                 {
-                    $_input = '<div class="input-group">
+                    $_input =  '<div class="form-group field-'.$InputId.'">
                                   <span class="input-group-addon" id="basic-addon1">'.$label1.'</span>
                                   '.Html::activeTextInput($model, $fieldname).'
+                                  <p class="help-block"></p>
                                 </div>';
                 }
             }
+
+
 
             return $_input;
 
